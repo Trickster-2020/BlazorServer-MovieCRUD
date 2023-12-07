9497c27235c92e7664b15a081fa0634e3b170ead
Sujan Karna <karna.sujan52@gmail.com>
2023-12-07 08:50:20 +0545

    Initial Commit

---
+++ MovieCRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MovieCRUD;
+
+namespace MovieCRUD.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<MovieCRUD.Movie> Movie { get; set; } = default!;
+    }
+}
